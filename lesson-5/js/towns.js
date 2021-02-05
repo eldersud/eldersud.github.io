fc93c2122c4022fc714c884179d9bea08d6cb5b4
eldersud <cua20002@byui.edu>
2021-02-05 12:21:44 -0500

    fixing

--- lesson-5/js/towns.js
+++ lesson-5/js/towns.js
@@ -6,23 +6,22 @@ fetch(requestURL)
       .then(function (jsonObject) {
         console.table(jsonObject);  
         const towns = jsonObject['towns'];
-
-        let town_numbers=[5,6, 1]
-        for (let i = 0; i < town_numbers.length; i++ ) 
+         let town=[5,6, 1]
+        for (let i = 0; i < town.length; i++ ) 
         {
-            let card = document.createElement('section');
+            let card= document.createElement('section');
             let h2 = document.createElement('h2');
-            h2.textContent = towns[town_numbers[i]].name;
+            h2.textContent = towns[town[i]].name;
             let motto=document.createElement('h3');
-            motto.textContent= towns[town_numbers[i]].motto;
+            motto.textContent= towns[town[i]].motto;
             let year =document.createElement('p');
-            year.textContent= "Year Founded: " + towns[town_numbers[i]].yearFounded;
+            year.textContent= "Year Founded: " + towns[town[i]].yearFounded;
             let population =document.createElement('p');
-            population.textContent= "Population: " + towns[town_numbers[i]].currentPopulation;
+            population.textContent= "Population: " + towns[town[i]].currentPopulation;
             let rain =document.createElement('p');
-            rain.textContent= "Annual Rain Fall: " + towns[town_numbers[i]].averageRainfall;
+            rain.textContent= "Annual Rain Fall: " + towns[town[i]].averageRainfall;
             let image=document.createElement('img');
-            image.setAttribute('src', "images/"+towns[town_numbers[i]].photo);
+            image.setAttribute('src', "images/"+towns[town[i]].photo);
             let statBlock = document.createElement('div');
             statBlock.className="info"
             let townName = document.createElement('div');
@@ -30,8 +29,6 @@ fetch(requestURL)
 
             townName.appendChild(h2);
             townName.appendChild(motto);
-
-            
             statBlock.appendChild(year);
             statBlock.appendChild(population);
             statBlock.appendChild(rain);
