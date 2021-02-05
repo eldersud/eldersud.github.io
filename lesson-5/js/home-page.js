043b535c7d0f199a6874ebe2d0fe0d903e474822
eldersud <cua20002@byui.edu>
2021-02-05 11:28:09 -0500

    still fixing

--- lesson-5/js/home-page.js
+++ lesson-5/js/home-page.js
@@ -1,13 +1,13 @@
 var currentYear = new Date().getFullYear();
-// console.log('currentYear: ', currentYear);
+
 document.getElementById("currentYear").innerHTML = currentYear.toString();
 
 
-//Getting the current date
+
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('lastup').textContent = new Date().toLocaleDateString('en-US', options);
 
-//Making the ham buttom works <<credits: https://codepen.io/blazzard-jason/pen/gmGxaj/>>
+
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.ul-nav')
 
--- lesson-5/js/towns.js
+++ lesson-5/js/towns.js
@@ -1,35 +1,44 @@
-
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
 fetch(requestURL)
       .then(function (response) {
         return response.json();
       })
       .then(function (jsonObject) {
-        console.table(jsonObject); 
-        const towns = jsonObject['towns']; 
-        for (let i = 0; i < towns.length; i++ ){
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
+
+        let town_numbers=[5,6, 1]
+        for (let i = 0; i < town_numbers.length; i++ ) 
+        {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
-            let h3= document.createElement('h3');
-            let p1= document.createElement('p');
-            let p2= document.createElement('p');
-            let p3= document.createElement('p');
-            let img = document.createElement('img');
-            h2.textContent = towns[i].name;
-            card.appendChild(h2);
-            h3.textContent = towns[i].motto;
-            card.appendChild(h3);
-            p1.textContent = 'Year Founded: ' +  towns[i].yearFounded;
-            card.appendChild(p1);
-            p2.textContent = 'Current Population: ' +  towns[i].currentPopulation;
-            card.appendChild(p2);
-            p3.textContent = 'Anual Rain Fall: ' +  towns[i].averageRainfall;
-            card.appendChild(p3);
-            img.setAttribute('src', towns[i].photo);
-            img.setAttribute('alt', towns[i].name);
-            card.appendChild(img);
-            document.querySelector('div.cards').appendChild(card);
+            h2.textContent = towns[town_numbers[i]].name;
+            let motto=document.createElement('h3');
+            motto.textContent= towns[town_numbers[i]].motto;
+            let span1 =document.createElement('p');
+            span1.textContent= "Year Founded: " + towns[town_numbers[i]].yearFounded;
+            let span2 =document.createElement('p');
+            span2.textContent= "Population: " + towns[town_numbers[i]].currentPopulation;
+            let span3 =document.createElement('p');
+            span3.textContent= "Annual Rain Fall: " + towns[town_numbers[i]].averageRainfall;
+            let image=document.createElement('img');
+            image.setAttribute('src', "images/"+towns[town_numbers[i]].photo);
+            let statBlock = document.createElement('div');
+            statBlock.className="info"
+            let townName = document.createElement('div');
+            townName.className="town_name"
 
+            townName.appendChild(h2);
+            townName.appendChild(motto);
+
+            
+            statBlock.appendChild(span1);
+            statBlock.appendChild(span2);
+            statBlock.appendChild(span3);
+            card.appendChild(townName);
+            card.appendChild(statBlock);
+            card.appendChild(image);
+
+            document.querySelector('div.cards').appendChild(card);
         }
-      });
+    });
