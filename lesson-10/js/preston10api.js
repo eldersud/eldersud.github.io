a059e4bb00146d0d22f84230ddec760f5a9325f3
eldersud <cua20002@byui.edu>
2021-02-08 10:11:28 -0500

    fixing errors

---
+++ lesson-10/js/preston10api.js
@@ -0,0 +1,77 @@
+// Actual variables
+const APIurl = 'https://api.openweathermap.org/data/2.5/';
+const weatherAPIurl = APIurl + 'weather';
+const forecastAPIurl = APIurl + 'forecast';
+const apiID = '&appid=727f53b8cdaee659ac2e713f75c01722';
+const apiUnits = '&units=imperial';
+const prestonURL = '?id=5604473' + apiID + apiUnits;
+
+
+// concatenating the links
+const prestonWeather = weatherAPIurl + prestonURL;
+const prestonForecast = forecastAPIurl + prestonURL;
+
+
+// Page selector
+let weatherURL = "";
+let forecastURL = "";
+if (document.getElementById("preston-body")) {
+    weatherURL = prestonWeather;
+    forecastURL = prestonForecast;
+} 
+
+
+// Weather functions
+fetch(weatherURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        let t = parseFloat(jsObject.main.temp);
+        let s = parseFloat(jsObject.wind.speed);
+        let output = "N/A";
+        const imagesrc = 'https://openweathermap.org/img/wn/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+        const desc = jsObject.weather[0].description; // reference the weather array
+        //gettin' the stuff!
+        document.getElementById('icon').setAttribute('src', imagesrc); // these two show icon
+        document.getElementById('icon').setAttribute('alt', desc);
+        document.getElementById('current').innerHTML = jsObject.weather[0].description;
+        document.getElementById('temp').innerHTML = Math.round(t) + "&#8457;";
+        if (t <= 50 && s >= 3) {
+            let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow(s, 0.16))));
+            output = Math.round(f);
+        };
+
+        document.getElementById('highTemp').innerHTML = jsObject.main.temp_max + "&#8457;";
+        document.getElementById("wind-chill").innerHTML = output + "&#8457;";
+        document.getElementById('humidity').innerHTML = jsObject.main.humidity;
+        document.getElementById('wind-speed').innerHTML = Math.round(s) + " MPH";
+    });
+    //5-day forecast
+fetch(forecastURL)
+    .then((response) => response.json())
+    .then((forecastObject) => {
+        var forecast = forecastObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+        console.table(forecast)
+        for (let day = 0; day < forecast.length; day++) {
+
+            //days of the week
+            let weatherDay = document.getElementsByClassName('day');
+            for (let i = 0; i < weatherDay.length; i++) {
+                let longDate = new Date(forecast[day].dt_txt);
+                weatherDay[day].textContent = longDate.toLocaleString("en-us", {
+                    weekday: "long"
+                });
+            }
+            //temperature
+            let forecastTemp = document.getElementsByClassName('forecastTemp');
+            for (let i = 0; i < forecastTemp.length; i++) {
+                forecastTemp[day].innerHTML = forecast[day].main.temp;
+            }
+            //icon
+            let weatherIcon = document.getElementsByClassName("forcastimg");
+            for (let i = 0; i < weatherIcon.length; i++) {
+                weatherIcon[day].setAttribute("src", `https://openweathermap.org/img/wn/${forecast[day].weather[0].icon}@2x.png`);
+                weatherIcon[day].setAttribute("alt", `Icon representing ${forecast[day].weather[0].description}`);
+            }
+        }
+
+    });
